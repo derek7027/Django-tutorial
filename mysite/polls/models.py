e6e70acae12581f7685b0bfed2dcacac69e62b26
derek7027 <derek.nunn@westpoint.edu>
2019-01-31 11:30:52 -0500

    Beginning of Tutorial 3

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,8 +1,12 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     def __str__(self):
         return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
